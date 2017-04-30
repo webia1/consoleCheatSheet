da0b008b70404b70764d030b2613eb1024214c36
Jan Nahody <jan.nahody@gmail.com>
2017-04-30 14:13:50 +0200

    Add table methos for local testing

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ function resizeArticle(size) {
         allArticles[i].style.fontSize = `${size}px`;
     }
 }
+
+function tableMethod() {
+    function Person(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    var family = {};
+    family.mother = new Person("Susan", "Doyle", 32);
+    family.father = new Person("John", "Doyle", 33);
+    family.daughter = new Person("Lily", "Doyle", 5);
+    family.son = new Person("Mike", "Doyle", 8);
+
+    console.table(family, ["firstName", "age"]);
+}
